f93d5503aebc9d1fce03fbf53cd245d868ce5c0e
Mateusz_Glejzer <mateusz.glejzer@fortedigital.no>
2022-05-12 22:24:05 +0200

    add userRepository

---
+++ BankApp/Users.Core/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Users.Core.Entities;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+
+    public User(string name, string surname, string email)
+    {
+        Name = name;
+        Surname = surname;
+        Email = email;
+        Id = new Guid();
+    }
+}
---
+++ BankApp/Users.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Users.Core.Entities;
+
+namespace Users.Core.Repositories;
+
+public interface IUserRepository
+{
+    Task AddAsync(User user);
+    Task ChangeAsync(User user);
+    Task DeleteAsync(User user);
+    Task GetAsync(User user);
+}
