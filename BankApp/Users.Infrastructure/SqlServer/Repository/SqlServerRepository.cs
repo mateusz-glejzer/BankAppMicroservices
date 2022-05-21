c625654299247cacb91a5979e128404dbdb97c23
Mateusz_Glejzer <mateusz.glejzer@fortedigital.no>
2022-05-21 11:28:14 +0200

    add dbcontext

---
+++ BankApp/Users.Infrastructure/SqlServer/Repository/SqlServerRepository.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Users.Core.Entities;
+using Users.Core.Exceptions;
+using Users.Core.Repositories;
+using Users.Infrastructure.SqlServer.Entities;
+
+namespace Users.Infrastructure.SqlServer.Repository;
+
+public class SqlServerRepository : IUserRepository
+{
+    private readonly UsersDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public SqlServerRepository(UsersDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task Create(User user)
+    {
+        if (await _dbContext.Users.FindAsync(user.Id) is not null)
+        {
+            throw new UserExistsException(user.Id);
+        }
+
+        var newUser = new UserEntity()
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Email = user.Email,
+        };
+
+        await _dbContext.Users.AddAsync(newUser);
+    }
+
+    public async Task ChangeAsync(User user)
+    {
+        var userInDb = await _dbContext.Users.FindAsync(user.Id);
+        if (userInDb is null)
+        {
+            throw new UserNotFoundException(user.Id);
+        }
+
+        
+        _dbContext.Users.Update();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var userInDb = await _dbContext.Users.FindAsync(id);
+        if (userInDb is null)
+        {
+            throw new UserNotFoundException(id);
+        }
+
+        
+        _dbContext.Users.Remove(userInDb);
+    }
+
+    public async Task<User> GetAsync(Guid id)
+    {
+        var userInDb = await _dbContext.Users.FindAsync(id);
+        if (userInDb is null)
+        {
+            throw new UserNotFoundException(id);
+        }
+
+        var mappedUser = _mapper.Map<User>(userInDb);
+        return mappedUser;
+    }
+}
---
+++ BankApp/Users.Infrastructure/SqlServer/UsersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users.Infrastructure.SqlServer.Entities;
+
+namespace Users.Infrastructure.SqlServer;
+
+public class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<UserEntity> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>()
+            .Property(user => user.Id)
+            .IsRequired();
+    }
+}
